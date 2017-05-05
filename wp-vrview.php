cf29f89d12409bb13db53c97d30be2ba02507439
Nícholas André <nicholas.andre@10up.com>
2017-05-04 22:48:48 -0300

    Properly registering and loading the plugin styles.

--- wp-vrview.php
+++ wp-vrview.php
@@ -2,42 +2,60 @@
 /*
 * Plugin Name: WP-VR-view - 360 photo/video
 * Description: Add 360 photos and  360 videos to wordpress pages, post, widgets etc. Google cardboard compatible. For additional information and usecases please visit demo website.
-* Version: 2.1
+* Version: 2.2
 * Author: Tumanov Alexander
 * Author URI: https://alexander-tumanov.name
 */
-require_once( __DIR__ . '/includes/php/autoload.php' );
-
-
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-add_shortcode('vrview', 'vr_creation');
+define( 'WP_NR_VERSION', '2.2' );
+define( 'WP_NR_URL', esc_url( plugin_dir_url( __FILE__ ), array( 'http', 'https' ) ) );
+define( 'WP_NR_PATH', wp_normalize_path( dirname( __FILE__ ) . '/' ) );
 
-add_action( 'wp_head', 'build_stylesheet_url' );
-function build_stylesheet_url() {
-    echo '<link rel="stylesheet" href="'. plugins_url().'/wp-vr-view/includes/css/style.css" type="text/css" media="all" />';
+require_once( __DIR__ . '/includes/php/autoload.php' );
+
+/**
+ * Register our stylesheet to be used later
+ *
+ * @return void
+ */
+function vr_register_style() {
+    wp_register_style(
+        'wp_nr_vr_style',
+	    WP_NR_URL . 'includes/css/style.css',
+        array(),
+	    WP_NR_VERSION
+    );
 }
+add_action( 'init', 'vr_register_style' );
 
-// Shortcode to display form on any page or post.
-function vr_creation($atts){
+/**
+ * Shortcode to display form on any page or post.
+ *
+ * @param $atts
+ *
+ * @return void
+ */
+function vr_creation( $atts ) {
+	wp_enqueue_style( 'wp_nr_vr_style' );
 
-    $a = shortcode_atts( array(
-       'img' => '',
-       'video' => '',
-       'pimg' => '',
-       'width' => '640',
-       'height' => '360',
-       'stereo' => 'false',
-       'yaw'=>0,
-       'hascontrols'=>'true'
-   ), $atts );
+	$a = shortcode_atts( array(
+		'img'         => '',
+		'video'       => '',
+		'pimg'        => '',
+		'width'       => '640',
+		'height'      => '360',
+		'stereo'      => 'false',
+		'yaw'         => 0,
+		'hascontrols' => 'true'
+	), $atts );
 
-   if ($a['video']) {
-     $vrVideo1= new VrVideo($a);
-     echo $vrVideo1->generateHtmlCode();
-   }
-   else{
-     $vrImage1= new VrImage($a);
-     echo $vrImage1->generateHtmlCode();
-   }
-  } ?>
+	if ( $a['video'] ) {
+		$vrVideo1 = new VrVideo( $a );
+		echo $vrVideo1->generateHtmlCode();
+	} else {
+		$vrImage1 = new VrImage( $a );
+		echo $vrImage1->generateHtmlCode();
+	}
+}
+add_shortcode( 'vrview', 'vr_creation' );
